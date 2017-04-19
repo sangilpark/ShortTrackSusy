c3a2901aec970bd205954d52fe5f5af20dd5dce4
sambein <samuel.bein@gmail.com>
2017-04-19 04:52:59 -0500

    added script to edit config file

---
+++ python/edit_config.py
@@ -0,0 +1,28 @@
+import os, sys
+
+FredBlurb = '''
+from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper
+randSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)
+randSvc.populate()
+#process.source.firstLiminosityBlock = cms.untracked.uint32(1 + seed)
+'''
+
+
+try: cname = sys.argv[1]
+except: cname = 'nonexistent.py'
+
+f = open(cname)
+lines = f.readlines()
+f.close()
+
+fnew = open('2'+cname,'w')
+armed = False
+for line in lines:
+    if 'process = ' in line: armed = True
+    fnew.write(line)
+    if armed:
+        fnew.write(FredBlurb)
+        armed = False
+    
+
+os.system('mv 2'+cname+' '+cname)
