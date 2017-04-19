07c58daecc181284912f9abdf91476ab36354fc2
sambein <samuel.bein@gmail.com>
2017-04-19 03:29:26 -0500

    added hadd script for benchmarks

---
+++ python/haddems.py
@@ -0,0 +1,13 @@
+import os
+
+os.system('hadd $EOS/store/user/sbein/LongLiveTheChi/LargeFiles/pMSSM12_MCMC1_27_200970_step2_AODSIM.root $EOS/store/user/sbein/LongLiveTheChi/aodsim/pMSSM12_MCMC1_27_200970_step2_AODSIM_*.root &')
+os.system('hadd $EOS/store/user/sbein/LongLiveTheChi/LargeFiles/pMSSM12_MCMC1_10_374794_step2_AODSIM.root $EOS/store/user/sbein/LongLiveTheChi/aodsim/pMSSM12_MCMC1_10_374794_step2_AODSIM_*.root &')
+os.system('hadd $EOS/store/user/sbein/LongLiveTheChi/LargeFiles/pMSSM12_MCMC1_12_865833_step2_AODSIM.root $EOS/store/user/sbein/LongLiveTheChi/aodsim/pMSSM12_MCMC1_12_865833_step2_AODSIM_*.root &')
+os.system('hadd $EOS/store/user/sbein/LongLiveTheChi/LargeFiles/pMSSM12_MCMC1_13_547677_step2_AODSIM.root $EOS/store/user/sbein/LongLiveTheChi/aodsim/pMSSM12_MCMC1_13_547677_step2_AODSIM_*.root &')
+os.system('hadd $EOS/store/user/sbein/LongLiveTheChi/LargeFiles/pMSSM12_MCMC1_20_690321_step2_AODSIM.root $EOS/store/user/sbein/LongLiveTheChi/aodsim/pMSSM12_MCMC1_20_690321_step2_AODSIM_*.root &')
+os.system('hadd $EOS/store/user/sbein/LongLiveTheChi/LargeFiles/pMSSM12_MCMC1_27_969542_step2_AODSIM.root $EOS/store/user/sbein/LongLiveTheChi/aodsim/pMSSM12_MCMC1_27_969542_step2_AODSIM_*.root &')
+os.system('hadd $EOS/store/user/sbein/LongLiveTheChi/LargeFiles/pMSSM12_MCMC1_28_737434_step2_AODSIM.root $EOS/store/user/sbein/LongLiveTheChi/aodsim/pMSSM12_MCMC1_28_737434_step2_AODSIM_*.root &')
+os.system('hadd $EOS/store/user/sbein/LongLiveTheChi/LargeFiles/pMSSM12_MCMC1_44_855871_step2_AODSIM.root $EOS/store/user/sbein/LongLiveTheChi/aodsim/pMSSM12_MCMC1_44_855871_step2_AODSIM_*.root &')
+os.system('hadd $EOS/store/user/sbein/LongLiveTheChi/LargeFiles/pMSSM12_MCMC1_47_872207_step2_AODSIM.root $EOS/store/user/sbein/LongLiveTheChi/aodsim/pMSSM12_MCMC1_47_872207_step2_AODSIM_*.root &')
+os.system('hadd $EOS/store/user/sbein/LongLiveTheChi/LargeFiles/pMSSM12_MCMC1_5_448429_step2_AODSIM.root $EOS/store/user/sbein/LongLiveTheChi/aodsim/pMSSM12_MCMC1_5_448429_step2_AODSIM_*.root &')
+os.system('hadd $EOS/store/user/sbein/LongLiveTheChi/LargeFiles/pMSSM12_MCMC1_8_373637_step2_AODSIM.root $EOS/store/user/sbein/LongLiveTheChi/aodsim/pMSSM12_MCMC1_8_373637_step2_AODSIM_*.root &')
