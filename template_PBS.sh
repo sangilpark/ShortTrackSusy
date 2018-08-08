fc1d24254ba790b2eb72d6048eb02c9317fe75ce
sangilpark <sangilpark@knu.ac.kr>
2018-08-08 17:35:09 +0900

    update

--- template_PBS.sh
+++ template_PBS.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#PBS -e /u/user/sangilpark/WorkDir/DisappearingTracks/jobs/log
-#PBS -o /u/user/sangilpark/WorkDir/DisappearingTracks/jobs/log
+#PBS -e /u/user/sangilpark/WorkDir/DisappearingTracks/jobs/logs
+#PBS -o /u/user/sangilpark/WorkDir/DisappearingTracks/jobs/logs
 
 # cms software setup
 export JOBDIR=/u/user/sangilpark/WorkDir/DisappearingTracks/jobs/SIGID_n0
+export OUTPUTDIR=/u/user/sangilpark/WorkDir/DisappearingTracks/jobs/outputs
 
 export CMSSW_BASE=/u/user/sangilpark/WorkDir/DisappearingTracks/CMSSW_7_1_25_patch1
 cd $CMSSW_BASE/src
 eval `scramv1 runtime -sh`
 
-if [ ! -d $JOBDIR]
+if [ ! -d $JOBDIR ]
 then 
     mkdir -p $JOBDIR
     cd $JOBDIR
@@ -28,12 +29,14 @@ cd $JOBDIR
 cmsDriver.py step1 --filein file:SIGID_step0_GENSIM_n0.root --fileout file:SIGID_step1_GENSIMRAW_n0.root --pileup_input dbs:/MinBias_TuneCUETP8M1_13TeV-pythia8/RunIISummer15GS-MCRUN2_71_V1-v2/GEN-SIM --mc --eventcontent RAWSIM --pileup 2015_25ns_FallMC_matchData_PoissonOOTPU --datatier GEN-SIM-RAW --conditions 80X_mcRun2_asymptotic_2016_TrancheIV_v6 --step DIGI,L1,DIGI2RAW,HLT:@frozen2016 --era Run2_2016 --python_filename SIGID_step1.py --customise Configuration/DataProcessing/Utils.addMonitoring,DisappTrks/SignalMC/genParticlePlusGeant.customizeKeep,DisappTrks/SignalMC/genParticlePlusGeant.customizeProduce -n NUMEVENTS
 
 cmsDriver.py step2 --filein file:SIGID_step1_GENSIMRAW_n0.root --fileout file:SIGID_step2_AODSIM_n0.root --mc --eventcontent AODSIM --runUnscheduled --datatier AODSIM --conditions 80X_mcRun2_asymptotic_2016_TrancheIV_v6 --step RAW2DIGI,RECO,EI --nThreads 4 --era Run2_2016 --python_filename AOD_cfg.py --customise Configuration/DataProcessing/Utils.addMonitoring,DisappTrks/SignalMC/genParticlePlusGeant.customizeProduce,DisappTrks/SignalMC/genParticlePlusGeant.customizeKeep -n NUMEVENTS
+cp SIGID_step2_AODSIM_n0.root $OUTPUTDIR
 #xrdcp SIGID_step2_AODSIM_n0.root root://cmseos.fnal.gov//store/user/lpcsusyhad/sbein/LongLiveTheChi/aodsim/smallchunks/
 
 cmsDriver.py step3 --conditions auto:run2_mc --fast --eventcontent MINIAODSIM --runUnscheduled --filein file:SIGID_step2_AODSIM_n0.root --fileout file:SIGID_step3_miniAODSIM_n0.root -s PAT --datatier MINIAODSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --customise Configuration/DataProcessing/Utils.addMonitoring,DisappTrks/SignalMC/genParticlePlusGeant.customizeProduce,DisappTrks/SignalMC/genParticlePlusGeant.customizeKeep --mc -n NUMEVENTS
+cp SIGID_step3_miniAODSIM_n0.root $OUTPUTDIR
 #xrdcp SIGID_step3_miniAODSIM_n0.root root://cmseos.fnal.gov//store/user/lpcsusyhad/sbein/LongLiveTheChi/miniaodsim/smallchunks/
 
 #rm *.py 
-#rm SIGID*.root
+rm -f SIGID*.root
 
 #try going back to scratch area before running code
